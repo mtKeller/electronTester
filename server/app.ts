ac01d8000472703e9bba82a2ce99d7a047037fec
Micah <micah.t.keller@gmail.com>
2019-11-05 11:16:39 -0800

    server crud wrap up

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,91 @@
 import express = require('express');
+import bodyParser = require('body-parser');
+
+interface device {
+  id: number;
+  name: string;
+  ip: string;
+  architecture: string;
+}
+
+interface Records{
+  devices: Array<device>;
+}
 
 // Create a new express application instance
 const app: express.Application = express();
+const RECORDS: Records = {
+  devices: []
+}
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// CREATE
+app.post('/record', function (req, res) {
+  if (req.body) {
+    let newRecord = req.body;
+    let exists = false;
+    for (let i = 0; i < RECORDS.devices.length; i++) {
+      if (RECORDS.devices[i].id === newRecord.id) {
+        res.send(false);
+        exists = true;
+        break;
+      }
+    }
+    if (!exists) {
+      RECORDS.devices.push(newRecord);
+      res.send(true);
+      console.log(RECORDS);
+    }
+  } else {
+    res.send(false);
+  }
+});
+//READ
+app.get('/record', function (req, res) {
+  res.send(JSON.stringify(RECORDS.devices));
+});
+//UPDATE
+app.put('/record', function (req, res) {
+  if (req.body) {
+    let newRecord = req.body;
+    let exists = false;
+    for (let i = 0; i < RECORDS.devices.length; i++) {
+      if (RECORDS.devices[i].id === newRecord.id) {
+        RECORDS.devices[i] = newRecord;
+        res.send(true);
+        exists = true;
+        break;
+      }
+    }
+    if (!exists) {
+      res.send(false);
+    }
+  } else {
+    res.send(false);
+  }
+  console.log(RECORDS);
+});
+//DELETE
+app.delete('/record', function (req, res) {
+  if (req.body) {
+    let newRecord = req.body;
+    let newDeviceList = [];
+    for (let i = 0; i < RECORDS.devices.length; i++) {
+      if (RECORDS.devices[i].id !== newRecord.id) {
+        newDeviceList.push(RECORDS.devices[i]);
+      }
+    }
+    RECORDS.devices = newDeviceList;
+    res.send(true);
+  } else {
+    res.send(false);
+  }
+  console.log(RECORDS);
 });
 
 app.listen(4333, function () {
--- server/dist/app.js
+++ server/dist/app.js
@@ -1,10 +1,81 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var bodyParser = require("body-parser");
 // Create a new express application instance
 var app = express();
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+var RECORDS = {
+    devices: []
+};
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// CREATE
+app.post('/record', function (req, res) {
+    if (req.body) {
+        var newRecord = req.body;
+        var exists = false;
+        for (var i = 0; i < RECORDS.devices.length; i++) {
+            if (RECORDS.devices[i].id === newRecord.id) {
+                res.send(false);
+                exists = true;
+                break;
+            }
+        }
+        if (!exists) {
+            RECORDS.devices.push(newRecord);
+            res.send(true);
+            console.log(RECORDS);
+        }
+    }
+    else {
+        res.send(false);
+    }
+});
+//READ
+app.get('/record', function (req, res) {
+    res.send(JSON.stringify(RECORDS.devices));
+});
+//UPDATE
+app.put('/record', function (req, res) {
+    if (req.body) {
+        var newRecord = req.body;
+        var exists = false;
+        for (var i = 0; i < RECORDS.devices.length; i++) {
+            if (RECORDS.devices[i].id === newRecord.id) {
+                RECORDS.devices[i] = newRecord;
+                res.send(true);
+                exists = true;
+                break;
+            }
+        }
+        if (!exists) {
+            res.send(false);
+        }
+    }
+    else {
+        res.send(false);
+    }
+    console.log(RECORDS);
+});
+//DELETE
+app.delete('/record', function (req, res) {
+    if (req.body) {
+        var newRecord = req.body;
+        var newDeviceList = [];
+        for (var i = 0; i < RECORDS.devices.length; i++) {
+            if (RECORDS.devices[i].id !== newRecord.id) {
+                newDeviceList.push(RECORDS.devices[i]);
+            }
+        }
+        RECORDS.devices = newDeviceList;
+        res.send(true);
+    }
+    else {
+        res.send(false);
+    }
+    console.log(RECORDS);
 });
 app.listen(4333, function () {
     console.log('Example app listening on port 4333!');
